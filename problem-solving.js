89e693dff9be880b5a3acf22376469cb6a16b34e
rhabir <rhabir.com>
2023-02-07 12:18:08 +0600

    added comment

--- problem-solving.js
+++ problem-solving.js
@@ -1,87 +1,120 @@
-// problem 1
 
 
-function mindGame(number){
-    /* This function do multiplication, addition subtraction and division. That will be return a number */
-    
+/* This function do multiplication, addition, subtraction 
+and division. That will be return a number */
+function mindGame(number) {
+
     const type = typeof number;
-    if(type === 'undefined' || type === 'string' || type === 'boolean' || type === 'function' || type === 'object' || type === 'symbol' || type === 'bigint'){
-        return 'please input Number'
+    if (type === 'undefined' || type === 'string' || type === 'boolean' 
+    || type === 'function' || type === 'object' ||
+    type === 'symbol' || type === 'bigint') {
+
+        return 'please input a Number'
     }
-    let multiSum = number * 3 + 10;
-    return multiSum / 2 - 5;
+    else {
+        let multiSum = number * 3 + 10;
+        return multiSum / 2 - 5;
+    }
 }
-// console.log(mindGame(33));
 
 
-// problem 2 
+/*This function is to check the 
+characters of a text are odd or even*/
+function evenOdd(text) {
 
-function evenOdd(text){
-    /*This function is to check the characters of a text are odd or even*/
-    
     let textLength = text.length;
-    let type = typeof text;
+    const type = typeof text;
 
-    if(type === 'undefined' || type === 'number' || type === 'boolean' || type === 'function' || type === 'object' || type === 'symbol' || type === 'bigint'){ 
+    if (type === 'undefined' || type === 'number' || type === 'boolean'
+     || type === 'function' || type === 'object' || type === 'symbol'
+    || type === 'bigint') {
+
         return 'please input Text';
     }
-    else if(textLength % 2 !== 1){
+    else if (textLength % 2 !== 1) {
         return 'even';
     }
-    else{
+    else {
         return 'odd';
     }
 }
-// console.log(evenOdd(12));
+// console.log(evenOdd(true));
 
-// problem 3 
 
-function isLGSeven(number){
-    /*This function is to check the Number is getter*/
-    let subtraction  = number - 7;
-    let type = typeof number;
 
-    if(type === 'undefined' || type === 'string' || type === 'boolean' || type === 'function' || type === 'object' || type === 'symbol' || type === 'bigint'){ 
-        return 'please input Number';
+/*This function do if number is greater than 7 that will be make 
+double input and less than 7 it will be return result*/
+function isLGSeven(number) {
+    let subtraction = number - 7;
+    const type = typeof number;
+
+    if (type === 'undefined' || type === 'string' || type === 'boolean' 
+    || type === 'function' || type === 'object' || type === 'symbol' 
+    || type === 'bigint') {
+
+        return 'please input a Number';
     }
-   else if(number >= 7){
+    else if (subtraction >= 7) {
         return number * 2;
     }
+
     return subtraction;
 }
-console.log(isLGSeven(10))
+// console.log(isLGSeven(13))
 
 
-// problem 4 
+ /*This function finding how many 
+ negative Number in an array*/
 
-function findingBadData(numbers){
+function findingBadData(numbers) {
+
+    const type = typeof numbers;
+
+    if (type === 'undefined' || type ==='string' 
+    || type === 'boolean' || type === 'function' 
+    || type === 'symbol' || type === 'bigint' 
+    || type === 'number') {
+        return 'please input an array of number';
+    }
+
     let badData = 0;
-    for(let number of numbers){
-        // console.log(number)
-        if(number < 0){
+    for (let number of numbers) {
+        if (number < 0) {
             badData++;
         }
     }
     return badData;
 }
 
-// console.log(findingBadData([1,2,5]))
+// console.log(findingBadData([ 2, -5, -7, -13  ]))
 
 
 // problem 5 
 
-function gemsToDiamond(friend1, friend2, friend3){
+function gemsToDiamond(friend1, friend2, friend3) {
     // console.log(friend1, friend2, friend3)
+
+    let typeFriend1 = typeof friend1,
+        typeFriend2 = typeof friend2,
+        typeFriend3 = typeof friend3;
+
+    if (typeFriend1 === 'undefined' || typeFriend2 === 'undefined' 
+    || typeFriend3 === 'undefined' || typeFriend1 === 'string' 
+    || typeFriend2 === 'string' || typeFriend3 === 'string') {
+
+        return 'please 3 input of number';
+    }
+
     let friend1Gems = friend1 * 21,
         friend2Gems = friend2 * 32,
         friend3Gems = friend3 * 43;
 
-    let totalGems = friend1Gems + friend2Gems + friend3Gems; 
+    let totalGems = friend1Gems + friend2Gems + friend3Gems;
 
-    if(totalGems >= 1000 * 2){
+    if (totalGems >= 1000 * 2) {
         return totalGems - 1000 * 2;
     }
     return totalGems;
 
 }
-// console.log(gemsToDiamond(100, 5, 1))
+console.log(gemsToDiamond( 1, 1, ))
